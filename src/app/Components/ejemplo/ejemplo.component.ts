b526675ce69ef195f606392cc1de6b18dc9e9014
Lorenzo <lorenzo.mendoza@gmail.com>
2019-09-10 16:40:50 -0500

    Se insertan primeros cambios

---
+++ src/app/Components/ejemplo/ejemplo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// Inyeccion
+@Component( {
+    selector:'app-ejemplo',
+    templateUrl:'ejemplo.component.html',
+    styleUrls: ['ejemplo.component.css']
+} )
+
+export class EjemploComponent {
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EjemploComponent } from './Components/ejemplo/ejemplo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //Etiquetas
+    EjemploComponent
   ],
   imports: [
     BrowserModule
